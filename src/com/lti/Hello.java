402e3f0722993be4316be5067c3b1d4f93b28e6c
pragya7277 <pragya.lnu@lntinfotech.com>
2020-09-14 12:14:09 +0530

    added date

--- src/com/lti/Hello.java
+++ src/com/lti/Hello.java
@@ -1,10 +1,18 @@
 package com.lti;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Worldddd.....");
+Date d =new Date();
+System.out.println(d);
+LocalDateTime dt=LocalDateTime.now();
+System.out.println(dt);
 	}
 
 }
